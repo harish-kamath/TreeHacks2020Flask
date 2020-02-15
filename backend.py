b46489e7ab53176cc1ca8511aa04d527897b64dd
hkamath3 <hkamath3@gatech.edu>
2020-02-15 14:34:16 -0800

    d

--- backend.py
+++ backend.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_cors import CORS, cross_origin
+import numpy as np
 app = Flask(__name__)
 
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-    return "Hello, World!"
+    if request.headers['Finished']:
+        print("Final Wav called!")
+        data = np.array(request.get_data())
+        np.save(data,'saved_numpy')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
