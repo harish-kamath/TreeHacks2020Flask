659f27bf6d59930749029d138b0b4f63f4267c16
hkamath3 <hkamath3@gatech.edu>
2020-02-15 18:31:14 -0800

    v

--- backend.py
+++ backend.py
@@ -1,18 +1,26 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_cors import CORS, cross_origin
 import numpy as np
+import scipy.io.wavfile as wavf
 from processWav import getAttribs
 app = Flask(__name__)
 
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
+current_partial = None
+
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.headers['Finished'] == 'true':
         print("Final Wav called!")
-        wavf.write('final.wav',44100,np.array([float(x) for x in request.get_data().decode('utf-8').split(',')]))
+        wavf.write('final.wav',44100,np.array([float(x) for x in current_partial.decode('utf-8').split(',')]))
+        current_partial = None
     else:
-        wavf.write('temp.wav',44100,np.array([float(x) for x in request.get_data().decode('utf-8').split(',')]))
+        if current_partial = None:
+            current_partial = request.get_data()
+        else:
+            current_partial += request.get_data()
+        wavf.write('temp.wav',44100,np.array([float(x) for x in current_partial.decode('utf-8').split(',')]))
         data = getAttribs("temp.wav")
         # Pitch, Jitter, Shimmer
         return str(data[0]) + "," + str(data[3]) + "," + str(data[8])
